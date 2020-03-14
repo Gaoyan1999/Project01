8257524c34aa75eba1f2793f3eb0fa5755478d72
Gaoyan1999 <1803483451@qq.com>
2020-03-14 20:50:17 +0800

    Initial commit

---
+++ src/com/java/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.java;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        ArrayList<Object> arrayList=new ArrayList<>();
+        arrayList.add("");
+        int i=3;
+        System.out.println(i);
+
+            method();
+
+
+
+    }
+
+
+
+
+    public static void  method(){
+
+    }
+}
